57917146b25958d7afd55205df2c32831a84b195
Lucas01PL <lucas01_ce@hotmail.com>
2019-04-06 20:30:02 -0300

    Add annotation @Repository in UsersRepository interface

--- src/main/java/com/hendisantika/adminlte/repository/UsersRepository.java
+++ src/main/java/com/hendisantika/adminlte/repository/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.hendisantika.adminlte.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hendisantika.adminlte.model.Users;
 
+@Repository("UsersRepository")
 public interface UsersRepository extends JpaRepository<Users, Long>{
 
     Users findByUsername(String username);
